2ee5df3adf6f9799bea18e6cf2fbe0f91605334e
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-08-09 22:33:34 -0400

    add hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
       colors: {
         slate: {
           1000: "#101010",
-          1100: '#151515'
-        }
-      }
+          1100: "#151515",
+        },
+        red: {
+          1000: "#FC0441"
+        },
+        gradient:
+          "linear-gradient(90deg, rgba(252,4,65,1) 0%, rgba(51,0,252,1) 100%)",
+      },
     },
   },
   plugins: [],
